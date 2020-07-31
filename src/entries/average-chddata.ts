a6ac570ce6b279d318ae68e22747cced1f190a81
陈志谦 <761208@nd.com>
2020-07-31 21:29:33 +0800

    1

--- src/entries/average-chddata.ts
+++ src/entries/average-chddata.ts
@@ -5,11 +5,11 @@ let shCodes = Modules.Database.database.codes.getSHCodes();
 let shKeys = Object.keys(shCodes);
 let codes = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 20; i++) {
     codes.push(shKeys[i]);
 }
 
-codes = ["603993"]
+// codes = ["603993"]
 async function main() {
     let prices = await Modules.Database.database.chddata.averagePrices(codes);
     console.log(prices)
