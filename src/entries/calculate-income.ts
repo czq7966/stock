aeec5193b31820b9af85219e9a094eb558d7cbd4
陈志谦 <761208@nd.com>
2020-08-13 17:58:53 +0800

    1

--- src/entries/calculate-income.ts
+++ src/entries/calculate-income.ts
@@ -40,21 +40,6 @@ async function start() {
         else 
             console.log(`${code} is out rule`)
     }
-
-    // let code = "600004";
-    // let investParams = await Services.Database.TransHis.calCodeInvestParams(Modules.Database.database.transhis,code, {
-    //         capital:{min: 3800, max: 4200},
-    //         income: {min: 80, max: 120}
-    //     })
-
-    // console.log(investParams)
-
-    // // investParams.prices.average = 16.6;
-    // // investParams.prices.high = 20.0;
-        
-    // let result = await Services.Database.TransHis.calCodeInvestment(Modules.Database.database.transhis, code, investParams);
-    // console.log(result)
-    
 }
 
 start()
