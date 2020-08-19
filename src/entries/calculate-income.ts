baa0b4e03f634eea98acf152d14beedb692eac6c
陈志谦 <761208@nd.com>
2020-08-19 11:11:39 +0800

    1

--- src/entries/calculate-income.ts
+++ src/entries/calculate-income.ts
@@ -6,19 +6,19 @@ import * as Services from '../services'
 async function start() {
     let investment: Modules.Dts.IInvestment = {maxCapital:50000, capital: {min:3800, max:4200}, income:{min:80, max:120}};
     let codes = Object.keys(Modules.Database.database.codes.getSHCodes());
-    Modules.Database.database.incomes.resetCSV();
     try {    
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 500; i++) {
             let  code = codes[i];
             let res = Modules.Database.database.incomes.getData(code) ||  await Services.Database.TransHis.getCodeInvestmentReturn(code, investment);
             if (res) {
-                // Modules.Database.database.incomes.setDataAndCSV(res.code, res);
+                Modules.Database.database.incomes.setData(res.code, res);
                 console.log(code)        
             }
             else 
                 console.log(`${code} is out rule`)
         }
     } finally {
+        // Modules.Database.database.incomes.resetCSV();        
         await Modules.Database.database.incomes.exportToCSV()
     }
 }
